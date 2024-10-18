4c11c149a78aa90e54c084472262a5056a9c281b
Danylo <dbashinskiy@miem.hse.ru>
2024-10-18 18:08:20 +0300

    Load by Asset Reference implemented

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -1,36 +1,46 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
 public class Bootstrap : MonoBehaviour
 {
-    [SerializeField] private ConfigsProvider _configsProvider;
     
     [Header("Player")] 
     [SerializeField] private PlayerSummoner _playerSummoner;
     [SerializeField] private Player _player;
+    [SerializeField] private AssetReference PlayerConfig;
     
     [Header("AppView")]
     [SerializeField] private List<InAppPackageView> _inAppPackageViews;
+    [SerializeField] private AssetReference AppConfig;
 
-    private void Awake()
+    private void Start()
     {
-        _configsProvider.OnConfigsLoaded += OnConfigsLoaded;
+        var playerConfigHandle = PlayerConfig.LoadAssetAsync<PlayerConfig>();
+        playerConfigHandle.Completed += OnPlayerConfigLoaded;
+        
+        var appConfigHandle= AppConfig.LoadAssetAsync<InAppPackageConfig>();
+        appConfigHandle.Completed += OnAppConfigLoaded;
     }
 
-    private void OnDisable()
+    private void OnPlayerConfigLoaded(AsyncOperationHandle<PlayerConfig> handle)
     {
-        _configsProvider.OnConfigsLoaded -= OnConfigsLoaded;
-    }
-
-    private void OnConfigsLoaded()
-    {
-        _player.Initialize(_configsProvider.GetRandomPlayerConfig());
-        _playerSummoner.SetConfig(_configsProvider.GetRandomPlayerConfig());
-
-        for (int i = 0; i < _inAppPackageViews.Count; i++)
+        if (handle.Status == AsyncOperationStatus.Succeeded)
         {
-            _inAppPackageViews[i].Initialize(_configsProvider.GetRandomAppConfig());
+            _player.Initialize(handle.Result);
+            _playerSummoner.SetConfig(handle.Result);
+        }
+    }
+    
+    private void OnAppConfigLoaded(AsyncOperationHandle<InAppPackageConfig> handle)
+    {
+        if (handle.Status == AsyncOperationStatus.Succeeded)
+        {
+            for (int i = 0; i < _inAppPackageViews.Count; i++)
+            {
+                _inAppPackageViews[i].Initialize(handle.Result);
+            }
         }
     }
 }
